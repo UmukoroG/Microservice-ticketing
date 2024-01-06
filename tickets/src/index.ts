633e4c2dc4851854e0821f6e6550984e3e1609e2
umukoroG <umukoroG>
2024-01-05 18:54:24 -0600

    starting up services to test deployment-workflow

--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -6,6 +6,7 @@ import { OrderCancelledListener } from './events/listeners/order-cancelled-liste
 import { app } from './app';
 
 const start = async () => {
+  console.log('Starting up tickets service...');
   if (!process.env.JWT_KEY) {
     throw new Error('JWT_KEY must be defined');
   }
