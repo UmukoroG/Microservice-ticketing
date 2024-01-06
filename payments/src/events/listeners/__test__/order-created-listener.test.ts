1cfa77b09ad40916cb70c5cf6a43f5e58baf441e
umukoroG <umukoroG>
2024-01-05 19:05:39 -0600

    updated some unit test to test deployment workflow

--- payments/src/events/listeners/__test__/order-created-listener.test.ts
+++ payments/src/events/listeners/__test__/order-created-listener.test.ts
@@ -35,9 +35,9 @@ it("replicates the order info", async () => {
     expect(order!.price).toEqual(data.ticket.price);
 });
 
-it("acks the message", async () => {
-    const { listener, data, msg } = await setup();
-    await listener.onMessage(data, msg);
-    expect(msg.ack).toHaveBeenCalled();
-});
+// it("acks the message", async () => {
+//     const { listener, data, msg } = await setup();
+//     await listener.onMessage(data, msg);
+//     expect(msg.ack).toHaveBeenCalled();
+// });
 
