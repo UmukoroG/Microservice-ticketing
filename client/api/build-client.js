ccf2c5ce3828f8b274cdcd9ec3d0cdc5175ba915
umukoroG <umukoroG>
2024-01-11 23:48:30 -0600

    starting up client

--- client/api/build-client.js
+++ client/api/build-client.js
@@ -2,14 +2,14 @@ import axios from 'axios';
 
 export default ({ req }) => {
   if (typeof window === 'undefined') {
-    // We are on the server
-
+    // We are on the local server
     // return axios.create({
     //   baseURL:
     //     'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local',
     //   headers: req.headers,
     // });
 
+    // We are on the cloud server
     return axios.create({
       baseURL: 'http://www.microservice-ticketing-sale-app.shop',
       headers: req.headers,
