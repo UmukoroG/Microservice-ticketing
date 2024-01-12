7d709a2d2d479fc6ea53a30d0ce8cf7695587a3d
umukoroG <umukoroG>
2024-01-12 01:17:03 -0600

    more css

--- client/pages/tickets/new.js
+++ client/pages/tickets/new.js
@@ -33,27 +33,31 @@ const NewTicket = () => {
 
   return (
     <div>
-      <h1>Create a Ticket</h1>
+      <h1 className='mt-8 text-center dark:text-dark-200'>Create a Ticket</h1>
       <form onSubmit={onSubmit}>
-        <div className="form-group">
+        <div className="w-full mt-4">
           <label>Title</label>
           <input
             value={title}
             onChange={(e) => setTitle(e.target.value)}
-            className="form-control"
+            placeholder='Title of the ticket'
+            className="block w-full px-4 py-2 mt-2 text-gray-700 placeholder-gray-500 bg-white border rounded-lg dark:bg-gray-800 dark:border-gray-600 dark:placeholder-gray-400 focus:border-blue-400 dark:focus:border-blue-300 focus:ring-opacity-40 focus:outline-none focus:ring focus:ring-blue-300"
           />
         </div>
-        <div className="form-group">
+        <div className="w-full mt-4">
           <label>Price</label>
           <input
             value={price}
+            placeholder='Price of the ticket'
             onBlur={onBlur}
             onChange={(e) => setPrice(e.target.value)}
-            className="form-control"
+            className="block w-full px-4 py-2 mt-2 text-gray-700 placeholder-gray-500 bg-white border rounded-lg dark:bg-gray-800 dark:border-gray-600 dark:placeholder-gray-400 focus:border-blue-400 dark:focus:border-blue-300 focus:ring-opacity-40 focus:outline-none focus:ring focus:ring-blue-300"
           />
         </div>
         {errors}
-        <button className="btn btn-primary">Submit</button>
+        <div className='flex items-center justify-between mt-4'>
+          <button className="px-6 py-2 text-sm font-medium tracking-wide text-white capitalize transition-colors duration-300 transform bg-blue-500 rounded-lg hover:bg-blue-400 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-50">Submit</button>
+        </div>
       </form>
     </div>
   );
