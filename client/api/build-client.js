0d31c25330d3a10312ce1b33aa70c78d6f70e153
umukoroG <umukoroG>
2024-01-21 19:39:15 -0600

    rerun service depl

--- client/api/build-client.js
+++ client/api/build-client.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export default ({ req }) => {
   if (typeof window === 'undefined') {
-    // We are on the local server
+    // We are on the local server.
     // return axios.create(
     //   {
     //     baseURL:'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local',
